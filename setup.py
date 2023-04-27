f8c8b41740b66a1b6cd9880c84430185a34dafc2
vincent d warmerdam <vincentwarmerdam@gmail.com>
2023-04-27 07:58:28 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ test_packages = ["pytest>=4.0.2"]
 setup(
     name="mktestdocs",
     version=__version__,
-    packages=find_packages(exclude=["notebooks"]),
+    packages=find_packages(exclude=["tests"]),
     install_requires=[],
     extras_require={
         "test": test_packages,
