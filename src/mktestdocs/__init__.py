afcff0d85d9562af1c0f82f561865628943d3742
koaning <vincentwarmerdam@gmail.com>
2025-07-25 14:07:25 +0200

    upgrade

--- src/mktestdocs/__init__.py
+++ src/mktestdocs/__init__.py
@@ -1,3 +1,4 @@
+import importlib.metadata
 from .__main__ import (
     register_executor,
     check_codeblock,
@@ -7,7 +8,10 @@ from .__main__ import (
     get_codeblock_members,
 )
 
+__version__ = importlib.metadata.version("mktestdocs")
+
 __all__ = [
+    "__version__",
     "register_executor",
     "check_codeblock",
     "grab_code_blocks",
